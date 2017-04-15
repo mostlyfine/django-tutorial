036469843a8fc87a27209b4ae92661859d0665bb
Seiji Sawayanagi <mostlyfine@gmail.com>
2017-04-16 01:17:43 +0900

    Add custom method.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
+import datetime
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 @python_2_unicode_compatible
 class Question(models.Model):
@@ -8,6 +10,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 @python_2_unicode_compatible
 class Choise(models.Model):
