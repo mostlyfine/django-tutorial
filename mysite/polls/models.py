8ceeb5e55af2c570b125075b9c2edb7743b2de20
Seiji Sawayanagi <mostlyfine@gmail.com>
2017-04-16 01:07:07 +0900

    Set object string.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date publised')
 
+    def __str__(self):
+        return self.question_text
+
+@python_2_unicode_compatible
 class Choise(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choise_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choise_text
 
